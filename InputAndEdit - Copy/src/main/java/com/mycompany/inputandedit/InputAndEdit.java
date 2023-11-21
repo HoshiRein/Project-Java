9d765e8b91a533b6fd16e7b96905868a4ed4c5f9
ReinHs <58736136+HoshiRein@users.noreply.github.com>
2023-11-21 20:39:47 +0700

    Add files via upload

---
+++ InputAndEdit - Copy/src/main/java/com/mycompany/inputandedit/InputAndEdit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inputandedit;
+
+/**
+ *
+ * @author ASUS
+ */
+public class InputAndEdit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
